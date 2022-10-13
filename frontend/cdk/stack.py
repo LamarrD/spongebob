423048a965ca4302cf258e6c83fb7c06c2fd433b
lamarrd <lamarr@stedi.com>
2022-10-12 23:07:11 -0400

    update cloudfront distribution to forward 403 to be handled by react router

--- frontend/cdk/stack.py
+++ frontend/cdk/stack.py
@@ -7,46 +7,63 @@ from aws_cdk import (
     aws_route53_targets as targets,
     aws_certificatemanager as acm,
     Stack,
-    CfnOutput
+    CfnOutput,
 )
 
 
 class FrontendStack(Stack):
     """A static website served via S3+CloudFront+Route53."""
-    def __init__(self, scope: Construct, id: str, domain_name: str, hosted_zone_id: str):
+
+    def __init__(
+        self, scope: Construct, id: str, domain_name: str, hosted_zone_id: str
+    ):
         super().__init__(scope, id)
 
         # Get a reference to the hosted zone AWS automatically creates when you register a domain
-        hosted_zone = route53.HostedZone.from_hosted_zone_attributes(self, 'hosted-zone',
-            hosted_zone_id=hosted_zone_id,
-            zone_name=domain_name
+        hosted_zone = route53.HostedZone.from_hosted_zone_attributes(
+            self, "hosted-zone", hosted_zone_id=hosted_zone_id, zone_name=domain_name
         )
-        
+
         # Create a certificate for the domain
-        certificate = acm.Certificate(self, "certificate",
+        certificate = acm.Certificate(
+            self,
+            "certificate",
             domain_name=domain_name,
-            validation=acm.CertificateValidation.from_dns(hosted_zone)
+            validation=acm.CertificateValidation.from_dns(hosted_zone),
         )
 
         # Create a plain bucket for the website to be hosted out of, note we do not name it or make it public
-        site_bucket = s3.Bucket(self, 'site-bucket')
+        site_bucket = s3.Bucket(self, "site-bucket")
 
         # Create a CloudFront distribution to sit in front of the bucket and actually serve the content
-        distribution = cloudfront.Distribution(self, "distribution",
+        distribution = cloudfront.Distribution(
+            self,
+            "distribution",
             certificate=certificate,
             default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(site_bucket), 
-                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+                origin=origins.S3Origin(site_bucket),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
             ),
             default_root_object="index.html",
             domain_names=[domain_name],
+            error_responses=[
+                cloudfront.ErrorResponse(
+                    http_status=403,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                )
+            ],
         )
 
         # Create an A record in Route53 so the CloudFront distribution can be accessed via the domain name
-        route53.ARecord(self, "alias-record",
-            target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)),
+        route53.ARecord(
+            self,
+            "alias-record",
+            target=route53.RecordTarget.from_alias(
+                targets.CloudFrontTarget(distribution)
+            ),
             zone=hosted_zone,
-            record_name=domain_name
+            record_name=domain_name,
         )
 
         # Create an output of the s3 bucket name so we can reference it when we deploy the frontend content to it
