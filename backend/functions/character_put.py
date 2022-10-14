8f35173d414ab382addef49b81605295f33c5f0e
lamarrd <lamarr@stedi.com>
2022-10-14 02:24:37 -0400

    add like dislike functionality for characters
    creating an interactive post request, ability to like and dislike characters

---
+++ backend/functions/character_put.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+import json
+from helper import DecimalEncoder
+
+
+table_name = os.getenv("TABLE_NAME")
+table = boto3.resource("dynamodb").Table(table_name)
+
+
+def handler(event, context):
+    """Increment character's likes/dislikes"""
+    character = event["pathParameters"]["character"]
+    increment = event["queryStringParameters"].get("increment") == "true"
+    decrement = event["queryStringParameters"].get("decrement") == "true"
+
+    response = table.get_item( Key={"pk": "character", "sk": event["pathParameters"]["character"]} )
+
+    if "Item" not in response:
+        return {
+            "statusCode": 404,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True,
+            },
+            "body": json.dumps({"error": "Character not found"}),
+        }
+
+    character = response["Item"]["data"]
+    if increment:
+        character["likes"] += 1
+    elif decrement:
+        character["likes"] -= 1
+
+    table.put_item( Item={"pk": "character", "sk": character["id"], "data": character} )
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        },
+        "body": json.dumps(character, cls=DecimalEncoder),
+    }
--- backend/functions/populate_db.py
+++ backend/functions/populate_db.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 from random import randrange
 
 
-table_name = os.getenv('TABLE_NAME')
-table = boto3.resource('dynamodb').Table(table_name)
+table_name = os.getenv("TABLE_NAME")
+table = boto3.resource("dynamodb").Table(table_name)
 
 
 # Data sourced from SpongeBob's Fandom Wiki
@@ -14,32 +14,44 @@ def handler(event, context):
     base_url = "https://spongebob.fandom.com"
     res = requests.get(f"{base_url}/wiki/List_of_characters/Main")
 
-    soup = BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.text, "html.parser")
     main_char_divs = soup.find_all("div", {"class": "bounceme"})
-    main_char_ids = [ main.attrs['id'] for main in main_char_divs ]
+    main_char_ids = [main.attrs["id"] for main in main_char_divs]
 
     for main_char_id in main_char_ids:
         char_link = f"{base_url}/wiki/{main_char_id}"
         res = requests.get(char_link)
-        char_soup = BeautifulSoup(res.text, 'html.parser')
+        char_soup = BeautifulSoup(res.text, "html.parser")
 
         data = {
             "id": main_char_id.lower(),
-            "full_name": char_soup.find_all('h2', {'data-source': 'name'})[0].text,
-            "residence": char_soup.find_all('div', {'data-source': 'residence'})[0].find_all('div')[0].text,
-            "job": char_soup.find_all('div', {'data-source': 'occupation(s)'})[0].find_all('div')[0].text,
-            "gender": char_soup.find_all('div', {'data-source': 'gender'})[0].find_all('div')[0].text,
-            "color": char_soup.find_all('div', {'data-source': 'color'})[0].find_all('div')[0].text,
-            "species": char_soup.find_all('div', {'data-source': 'species'})[0].find_all('div')[0].text,
-            "default_image": char_soup.find_all('div', {'data-source': 'image'})[0].find_all('img')[0].attrs['src'],
+            "full_name": char_soup.find_all("h2", {"data-source": "name"})[0].text,
+            "residence": char_soup.find_all("div", {"data-source": "residence"})[0]
+            .find_all("div")[0]
+            .text,
+            "job": char_soup.find_all("div", {"data-source": "occupation(s)"})[0]
+            .find_all("div")[0]
+            .text,
+            "gender": char_soup.find_all("div", {"data-source": "gender"})[0]
+            .find_all("div")[0]
+            .text,
+            "color": char_soup.find_all("div", {"data-source": "color"})[0]
+            .find_all("div")[0]
+            .text,
+            "species": char_soup.find_all("div", {"data-source": "species"})[0]
+            .find_all("div")[0]
+            .text,
+            "default_image": char_soup.find_all("div", {"data-source": "image"})[0]
+            .find_all("img")[0]
+            .attrs["src"],
             "link": char_link,
             "gallery_link": f"{base_url}/wiki/{main_char_id}/gallery",
-            "likes": str(randrange(1000)),
-            "dislikes": str(randrange(1000))
+            "likes": randrange(1000),
         }
         # Add to DynamoDB
-        table.put_item(Item={ "pk": "character", "sk": main_char_id.lower(), "data": data })
-
+        table.put_item(
+            Item={"pk": "character", "sk": main_char_id.lower(), "data": data}
+        )
 
 
 if __name__ == "__main__":
